20fd173ea2c033c00f34b34a9a3d31cf1bdd4f90
Larinenok <olegins03@gmail.com>
2022-12-16 17:23:06 +0700

    🤥🤥🤥

--- src/App.tsx
+++ src/App.tsx
@@ -4,48 +4,20 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
-
-  return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <div class="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+  
+    return (
+        <div class="main">
+            <div class="header centered">
+                <h3>Time</h3>
+            </div>
+            <div class="work centered">
+                <h3>Work</h3>
+            </div>
+            <div class="unwork centered">
+                <h3>Procrastination</h3>
+            </div>
         </div>
-      </div>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+    );
 }
 
 export default App;
